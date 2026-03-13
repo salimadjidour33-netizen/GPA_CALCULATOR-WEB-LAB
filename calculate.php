31568c862f64c5828f3f8caa60639576af2f2b51
salimadjidour33-netizen <salimadjidour33@gmail.com>
2026-03-13 11:15:45 +0100

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -1,35 +1,35 @@
 1 <? php
-2 if ( isset ( $_POST [’course ’] , $_POST [’credits ’] , $_POST [’grade ’]) ) {
-3 $courses = $_POST [’course ’];
-4 $credits = $_POST [’credits ’];
-5 $grades = $_POST [’grade ’];
-6 $totalPoints = 0;
-7 $totalCredits = 0;
-8
-9 echo "<table >";
-10 echo "<tr >
-11 <th > Course </th >
-12 <th > Credits </th >
-13 <th >Grade </th >
-14 <th > Grade Points </th >
-15 </tr >";
-16
-17 for ( $i = 0; $i < count ( $courses ) ; $i ++) {
-18 $course = htmlspecialchars ( $courses [ $i ]) ;
-19 $cr = floatval ( $credits [ $i ]) ;
-20 $g = floatval ( $grades [ $i ]) ;
-21 if ( $cr <= 0) continue ;
-22 $pts = $cr * $g ;
-23 $totalPoints += $pts ;
-24 $totalCredits += $cr ;
-25 echo "<tr >
-26 <td > $course </td >
-27 <td >$cr </td >
-28 <td >$g </td >
-29 <td >$pts </td >
+2 header (’Content - Type : application / json ’) ;
+3
+4 if ( isset ( $_POST [’course ’] , $_POST [’credits ’] , $_POST [’grade ’]) ) {
+5 $courses = $_POST [’course ’];
+6 $credits = $_POST [’credits ’];
+7 $grades = $_POST [’grade ’];
+8 $totalPoints = 0;
+9 $totalCredits = 0;
+10
+11 $tableHtml = ’<table class =" table table - bordered mt -3" > ’;
+12 $tableHtml .= ’<thead class =" thead - dark " >
+13 <tr >
+14 <th > Course </th > <th > Credits </th >
+15 <th >Grade </th > <th > Grade Points </th >
+16 </tr >
+17 </thead > <tbody >’;
+18
+19 for ( $i = 0; $i < count ( $courses ) ; $i ++) {
+20 $course = htmlspecialchars ( $courses [ $i ]) ;
+21 $cr = floatval ( $credits [ $i ]) ;
+22 $g = floatval ( $grades [ $i ]) ;
+23 if ( $cr <= 0) continue ;
+24 $pts = $cr * $g ;
+25 $totalPoints += $pts ;
+26 $totalCredits += $cr ;
+27 $tableHtml .= "<tr >
+28 <td > $course </td > <td >$cr </td >
+29 <td >$g </td > <td >$pts </td >
 30 </tr >";
 31 }
-32 echo " </table >";
+32 $tableHtml .= ’ </tbody > </ table >’;
 33
 34 if ( $totalCredits > 0) {
 35 $gpa = $totalPoints / $totalCredits ;
@@ -41,13 +41,27 @@
 41 $interpretation = " Pass ";
 42 } else {
 43 $interpretation = " Fail ";
-44 }
-45 echo "<p> Your GPA is <strong >" . number_format ( $gpa , 2)
-46 . " </strong > ( $interpretation ). </p>";
-47 } else {
-48 echo "<p>No valid courses entered . </p>";
-49 }
-50 } else {
-51 echo " Data not received .";
-52 }
-53 ? >
+  44 }
+45 $message = " Your GPA is " . number_format ( $gpa , 2)
+46 . " ( $interpretation ).";
+47 echo json_encode ([
+48 ’success ’ = > true ,
+49 ’gpa ’ = > $gpa ,
+50 ’message ’ = > $message ,
+51 ’tableHtml ’ = > $tableHtml ,
+52 ]) ;
+53 } else {
+54 echo json_encode ([
+55 ’success ’ = > false ,
+56 ’message ’ = > ’No valid courses entered .’,
+57 ]) ;
+58 }
+59 } else {
+60 echo json_encode ([
+61 ’success ’ = > false ,
+62 ’message ’ = > ’Data not received .’,
+63 ]) ;
+64 }
+65 exit ;
+66 ? >
+  
